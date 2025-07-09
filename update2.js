be582749239e0cf0528e9c04e50a2940de588f5a
1 <153518609+noobsblock@users.noreply.github.com>
2025-07-09 14:37:00 +0800

    Update update2.js

--- update2.js
+++ update2.js
@@ -9,21 +9,28 @@ class update {
                     blockType: 'button',
                     text: '更新'
                 },
-                { opcode: 'before',blockType:'command',text:'更新前的块' }
+                { opcode: 'before',blockType:'command',text:'更新前的块' }，
+                { opcode: 'after',blockType:'command',text:'更新后的块' }，
             ]
         }
     }
     async before(){
         alert('运行了更新之前的块')
     }
-    update() {
-    var pbak = prompt;
-    window.prompt = function() {return ""};
-    document.querySelector('[data-tip="扩展库"]').click();
-    var exts = document.querySelectorAll('.gandi_library_extensionItemCard_2cnXl');
-    var custom_ext = exts[exts.length - 1];
-    custom_ext.querySelector("button").click();
-    window.prompt = pbak;
-    document.querySelector('[data-tip="代码"]').click();
-  }
+    
+    async update() {
+        var pbak = prompt;
+        window.prompt = function() {return ""};
+        document.querySelector('[data-tip="扩展库"]').click();
+        var exts = document.querySelectorAll('.gandi_library_extensionItemCard_2cnXl');
+        var custom_ext = exts[exts.length - 1];
+        custom_ext.querySelector("button").click();
+        window.prompt = pbak;
+        document.querySelector('[data-tip="代码"]').click();
+    }
+    
+    async after() {
+        alert('运行了更新之后的块')
+    }
 }
+Scratch.extensions.register(new update());
