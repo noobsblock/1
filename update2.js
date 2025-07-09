9e39e895a6f15e60fc58daa5af97dda468723a90
1 <153518609+noobsblock@users.noreply.github.com>
2025-07-09 14:30:15 +0800

    Create update2.js

---
+++ update2.js
@@ -0,0 +1,29 @@
+class update {
+    getInfo() {
+        return {
+            id: 'update',
+            name: '更新示例',
+            blocks: [
+                {
+                    opcode: 'update',
+                    blockType: 'button',
+                    text: '更新'
+                },
+                { opcode: 'before',blockType:'command',text:'更新前的块' }
+            ]
+        }
+    }
+    async before(){
+        alert('运行了更新之前的块')
+    }
+    update() {
+    var pbak = prompt;
+    window.prompt = function() {return ""};
+    document.querySelector('[data-tip="扩展库"]').click();
+    var exts = document.querySelectorAll('.gandi_library_extensionItemCard_2cnXl');
+    var custom_ext = exts[exts.length - 1];
+    custom_ext.querySelector("button").click();
+    window.prompt = pbak;
+    document.querySelector('[data-tip="代码"]').click();
+  }
+}
